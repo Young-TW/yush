781213c7450edca40abed2048cd5ec877b3748ed
Young-TW <young20050727@gmail.com>
2023-02-05 04:53:32 +0800

    fix ls command dir color

--- src/commands/ls.cpp
+++ src/commands/ls.cpp
@@ -18,7 +18,7 @@ int cmd::ls(const std::vector<std::string>& arg, StreamManager& stream_manager, 
     for (auto& it : list) {
         if (it.path().filename().string()[0] != '.') {
             if (it.is_directory()) {
-                stream_manager.out() << variable_manager.get("COLOR_DIR") << it.path().filename().string() << '/' << variable_manager.get("COLOR_RESET");
+                stream_manager.out() << variable_manager.get("COLOR_DIR") << it.path().filename().string() << variable_manager.get("COLOR_RESET") << '/';
             } else {
                 stream_manager.out() << it.path().filename().string();
             }
@@ -38,7 +38,7 @@ int cmd::la(const std::vector<std::string>& arg, StreamManager& stream_manager, 
     std::filesystem::directory_iterator list(current_path);
     for (auto& it : list) {
         if (it.is_directory()) {
-            stream_manager.out() << variable_manager.get("COLOR_DIR") << it.path().filename().string() << '/' << variable_manager.get("COLOR_RESET");
+            stream_manager.out() << variable_manager.get("COLOR_DIR") << it.path().filename().string() << variable_manager.get("COLOR_RESET") << '/';
         } else {
             stream_manager.out() << it.path().filename().string();
         }
