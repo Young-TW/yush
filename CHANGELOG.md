f3ebc39971e08d926d893c835ddc9c8acd676e60
young <young20050727@gmail.com>
2023-06-08 06:56:27 +0800

    feat: main.cpp cxxopts support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [0.3.3] - 2023-06-08
+### Added
+- `main.cpp` cxxopts support
+
 ## [0.3.2] - 2023-06-08
 ### Fixed
 - Fix Github Actions workflow
@@ -36,6 +40,7 @@ Remove Windows support after this version
 ### Added
 - Initial release
 
+[0.3.3]: https://github.com/Young-TW/yush/releases/tag/v0.3.3
 [0.3.2]: https://github.com/Young-TW/yush/releases/tag/v0.3.2
 [0.3.1]: https://github.com/Young-TW/yush/releases/tag/v0.3.1
 [0.3.0]: https://github.com/Young-TW/yush/releases/tag/v0.3.0
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
 #include <fstream>
 #include <iostream>
+#include <filesystem>
 #include <cxxopts.hpp>
 #include "shell.h"
 
 int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        return Shell(std::cin, std::cout, std::cerr).run(true);
+    cxxopts::Options options("shell", "Young's shell");
+
+    options.add_options()
+        ("script"        , "input script file"                    , cxxopts::value<std::filesystem::path>())
+        ("o,debug-output", "Enable debug message output to a file", cxxopts::value<std::filesystem::path>())
+        ("h,help"        , "Print help message and exit"          , cxxopts::value<bool>()->default_value("false"))
+        ("c,command"     , "Execute single command"               , cxxopts::value<std::string>())
+        // ("i,interactive" , "Is interactive mode"                  , cxxopts::value<bool>()->default_value("true"))
+        ("v,version"     , "Print version and exit"               , cxxopts::value<bool>()->default_value("false"))
+        ;
+
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
     }
 
-    if (argc >= 2) {
-        std::ifstream fin(argv[1]);
-        return Shell(std::cin, std::cout, std::cerr).run(false);
+    if (result.count("version")) {
+        std::cout << "yush, version 0.3.3" << std::endl;
+        return 0;
     }
 
-    return 1;
+    if (result.count("debug-output")) {
+        std::ofstream fout(result["debug-output"].as<std::filesystem::path>());
+        return Shell(std::cin, std::cout, fout).run(false);
+    }
+
+    if (result.count("script")) {
+        std::ifstream fin(result["script"].as<std::filesystem::path>());
+        return Shell(fin, std::cout, std::cerr).run(false);
+    }
+
+    return Shell(std::cin, std::cout, std::cerr).run(true /*result.count("interactive")*/);
 }
