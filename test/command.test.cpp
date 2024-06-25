0eb2c0ed541e7c8e9ecbb4c7f2aa242b853fa065
young <young20050727@gmail.com>
2024-06-25 17:22:43 +0800

    ci/cd: enable gtest
    Co-authored-by: Lumynous <lumynou5.tw@gmail.com>

---
+++ test/command.test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+#include "command.h"
+
+TEST(Command, parse) { EXPECT_EQ(1, 1); };
--- test/shell.test.cpp
+++ test/shell.test.cpp
@@ -1,3 +1,5 @@
 #include <gtest/gtest.h>
 
+#include "shell.h"
+
 TEST(Shell, run) { EXPECT_EQ(1, 1); }
