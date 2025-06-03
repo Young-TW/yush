f94a4d67893121f35fe8be552e111ef85d1c8c40
Young <young20050727@gmail.com>
2025-06-03 10:55:56 +0800

    feat: support multiple path in ls command

--- src/commands/ls.cpp
+++ src/commands/ls.cpp
@@ -7,6 +7,32 @@
 #include "shell.h"
 
 int Shell::cmd_ls(const std::vector<std::string>& arg) {
+    // parse arguments if path parameters are given
+    if (arg.size() > 1) {
+        for (size_t i = 1; i < arg.size(); ++i) {
+            if (!std::filesystem::exists(arg[i])) {
+                fmt::print(stderr, "Path '{}' does not exist.\n", arg[i]);
+                return 1;
+            }
+
+            fmt::print("Listing directory: {}\n", arg[i]);
+            for (const auto& entry : std::filesystem::directory_iterator(arg[i])) {
+                if (entry.path().filename().string()[0] != '.') {
+                    if (entry.is_directory()) {
+                        fmt::print(fg(fmt::color::cyan), "{}", entry.path().filename().string());
+                        fmt::print("/");
+                    } else {
+                        fmt::print(fg(fmt::color::white), "{}", entry.path().filename().string());
+                    }
+                    fmt::print("\t");
+                }
+            }
+
+            fmt::print("\n");
+        }
+        return 0;
+    }
+
     if (!std::filesystem::exists(std::filesystem::current_path())) {
         fmt::print(stderr, "This directory is not exists.\n");
         return 1;
